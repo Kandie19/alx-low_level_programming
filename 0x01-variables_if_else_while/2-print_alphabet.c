b5fd5940815510c7718f14c5e1b18916dee73d42
Kandie19 <kandiemasasabi@gmail.com>
2023-06-19 00:53:50 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+char alp[26] = "abcdefghijklmnopqrstuvwxyz"; int i;
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i])
+}
+putchar('\n');
+return (0); 
+}
